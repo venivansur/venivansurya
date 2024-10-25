85f539550dbaf7da3085d9d8178c50bac16df6e7
unknown <vansurya.01@gmail.com>
2024-10-25 21:36:31 +0700

    tugas week 2

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,4 @@
-const hamMenu = document.querySelector('.ham-menu');
-
-
-const offScreenMenu = document.querySelector('.off-screen-menu');
-
-
-hamMenu.addEventListener('click',() => {
-    hamMenu.classList.toggle('active');
-})
+function hamburgerToggle() {
+    const hamburgerContainer = document.getElementById("hamburger-container")
+    hamburgerContainer.classList.toggle("hide")
+}
