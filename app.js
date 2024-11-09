03aaccc19d62e79aa02f3f546fec95f88580bfa7
unknown <vansurya.01@gmail.com>
2024-11-09 17:00:30 +0700

    fix deploy

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const app = express();
 const port = 3000;
 const path = require("path");
 require("./src/libs/hbs-helper");
-const config = require("./config/config.json")
+const config = require("./config/config")
 const {Sequelize, QueryTypes} = require("sequelize");
-const sequelize = new Sequelize(config.development)
+
 const bcrypt = require ("bcrypt");
 const session = require("express-session");
 const flash = require("express-flash");
 const upload = require("./src/middlewares/upload-file");
 
+require("dotenv").config()
+  const environment = process.env.NODE_ENV
 
-
+const sequelize = new Sequelize(config[environment]);
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./src/views"))
