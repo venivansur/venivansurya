938c127b471ef16a86b6d044bed90358621f57fe
unknown <vansurya.01@gmail.com>
2024-11-10 19:02:34 +0700

    finall

--- app.js
+++ app.js
@@ -10,28 +10,12 @@ const bcrypt = require ("bcrypt");
 const session = require("express-session");
 const flash = require("express-flash");
 const upload = require("./src/middlewares/upload-file");
-const pg = require("pg");
+
 require("dotenv").config()
-const environment = process.env.NODE_ENV || 'development';
+  const environment = process.env.NODE_ENV
 
+const sequelize = new Sequelize(config[environment]);
 
-const sequelize = new Sequelize(config[environment].database, config[environment].username, config[environment].password, {
-  host: config[environment].host,
-  dialect: config[environment].dialect,
-  dialectModule: pg,
-  dialectOptions: {
-      ssl: false // Menonaktifkan SSL
-  },
-});
-
-// Cek koneksi
-sequelize.authenticate()
-  .then(() => {
-      console.log('Koneksi ke database berhasil.');
-  })
-  .catch(err => {
-      console.error('Tidak dapat terhubung ke database:', err);
-  });
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./src/views"))
 app.use("/assets", express.static(path.join(__dirname, "./src/assets")));
